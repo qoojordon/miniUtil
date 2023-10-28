a6dac5e05b8ce010d1e717cdfc67b2ae8a1d1d3f
qoojordon <qoojordon@gmail..com>
2023-10-28 21:46:27 +0800

    1. separate util functions, include: 	-msg level handle 		a. error_out 		b. collect warnings
    2. make current capabilty clear by defining some global Tuples
    
    3. add err handle and make possible failures more clear
    	a. wrong file name
    	b. file w/o permission
    	c. head row does not statisfy spec
    	d. reduce execution of workbook.save(). still get permission error when call workbook.save() frequently

--- cal_kaywords/cal.py
+++ cal_kaywords/cal.py
@@ -1,89 +1,120 @@
 import sys
+from typing import Tuple
 print("start procedure...")
 import openpyxl
+
+RESULT_SHEET_NAMES=("Result_Author_Keyword", "Result_Keyword_Plus")
+SUPPORTED_FIELDS=('Article Title', 'Author Keywords', 'Keywords Plus')
+FEQ_FIELDS      =                 ('Author Keywords', 'Keywords Plus')
+g_field_2_idx = {}
+g_f_2_kw_map_feq = {}
+g_warnings = []
+
+def error_exit(msg: str):
+    print(f"[ERROR] {msg}")
+    sys.exit(1)
+
+def pwarn(msg: str):
+    suppres_upper_bound = 10
+    if len(g_warnings) < suppres_upper_bound:
+        print(f"[WARN] {msg}")
+    g_warnings.append(msg)
+    if len(g_warnings) == suppres_upper_bound:
+        print(f"[WARN] hit over {suppres_upper_bound} warnings, suppress later warnings")
+
+
+def verify_head_row_and_build_global_var(row : Tuple):
+    for idx, cell in enumerate(row):
+        if cell in SUPPORTED_FIELDS:
+            g_field_2_idx[cell] = idx
+    
+    if len(g_field_2_idx) != len(SUPPORTED_FIELDS):
+        error_exit(f"did not find all of required fields in heading row, given:{g_field_2_idx.keys()}, expected{SUPPORTED_FIELDS}")
+
+    for key, idx in g_field_2_idx.items():
+        print(f" key '{key:20}' at idx '{idx}'")
+    
+    print(f'build entry for fields which require to calculate frequency: {FEQ_FIELDS}')
+    for field in FEQ_FIELDS:
+        g_f_2_kw_map_feq[field] = {}
+
+def proc_one_row(row: Tuple):
+    #should handle by keyword
+    for field in FEQ_FIELDS:
+        f_cell = row[ g_field_2_idx[field] ]
+        if not f_cell:
+            raise Exception(f"empty '{field}'")
+        
+        feq_entry = g_f_2_kw_map_feq[field]
+        kw_list = [k.strip() for k in f_cell.split(';')]
+        for kw in kw_list:
+            lkw = kw.lower()
+            if lkw not in feq_entry.keys():
+                feq_entry[lkw] = 1
+            else:
+                feq_entry[lkw] += 1
+
+def save_keyword_feq_to_result_sheet(wb):
+    for field in FEQ_FIELDS:
+        result_sheet = wb.create_sheet(f"Result {field}")
+
+        for pair in g_f_2_kw_map_feq[field].items():
+            result_sheet.append(pair)
+
+    result_sheet = wb.create_sheet("Warnings")
+    for warn in g_warnings:
+        result_sheet.append((warn,))
+    try:
+        print(f"update result to result sheet")
+        wb.save(FILE_NAME)
+    except Exception as e:
+        print(f"there is something wrong when updating same file. wrtie result to another file named 'planB.xlsx'")
+        wb.save('planB.xlsx')
+    #wb.save(FILE_NAME)
 
 '''
 Requirement:
     1. filename
     2. to-be-analyzed sheet should named 'tb1'
 '''
-
-
 #read xls
 #TODO: handle file extention error
-print("reading xls'x'.....")
-FILE_NAME = "C:\\Users\wkroh\\Documents\\vscode_github\miniUtil\\miniUtil\\cal_kaywords\\full.xlsx"
-
-RESULT_SHEET_NAMES=("Result_Author_Keyword", "Result_Keyword_Plus")
+FILE_NAME = "C:\\Users\\qoojo\\OneDrive\\文件\\github\\miniUtil\\cal_kaywords\\full.xlsx"
+print(f"reading xlsx:{FILE_NAME}")
 
 try:
     wb = openpyxl.load_workbook(FILE_NAME)
     print("reading workbook successfully")
+except PermissionError as e:
+    error_exit(f"You probabaly opened {FILE_NAME} by MS Excel. ErrCode:{repr(e)}. Please close other programs which open FILE_NAME.")
+except FileNotFoundError as e:
+    error_exit(f"Your FILE_NAME '{FILE_NAME}' does not exist. ErrCode:{repr(e)}. Please correctify your FILE_NAME")
 except Exception as e:
-    print(f"[Error] Your FILE_NAME '{FILE_NAME}' does not exist. ErrCode:{repr(e)}")
-    sys.exit(1)  
-try:
-    wb.save(FILE_NAME)
-except Exception as e:
-    print(f"[Error] You probabaly opened {FILE_NAME} by MS Excel. ErrCode:{repr(e)}")
-    sys.exit(1)
+    print(f"e type: {type(e)}")
+    error_exit(f"hit unhandled exception when loading FILE_NAME '{FILE_NAME}'. ErrCode:{repr(e)}")
 
-for rst in RESULT_SHEET_NAMES:
-    if rst in wb.sheetnames:
-        print('[Warn] previous result will be removed')
-        wb.remove(wb[rst])
-        wb.save(FILE_NAME)
+sheet_names = [f'Result {field}' for field in FEQ_FIELDS]
+sheet_names.append('Warnings')
+dsn = [name for name in sheet_names if name in wb.sheetnames]
+for name in dsn:
+    pwarn(f"previous result sheet '{name}' will be removed")
+    wb.remove(wb[name])
+# wb.save(FILE_NAME)
 
+print(f"ATTENTION ! only sheet named 'tb1' will be analyzed")
 tb = wb['tb1']
-
-article_title_idx=-1
-author_keywords_idx=-1
-keywords_plus_idx=-1
-g_kw_feq = {}
-g_kwp_feq = {}
 
 for row_idx, row in enumerate(tb.iter_rows(values_only=True), start=1):
     if row_idx == 1:
-        for idx, cell in enumerate(row):
-            if cell == 'Article Title':
-                article_title_idx = idx
-            elif cell == 'Author Keywords':
-                author_keywords_idx = idx
-            elif cell == 'Keywords Plus':
-                keywords_plus_idx = idx
-        print(f"article_title_idx: {article_title_idx}, author_keywords_idx:{author_keywords_idx}, keywords_plus_idx:{keywords_plus_idx}")
+        key_2_idx = verify_head_row_and_build_global_var(row)
         continue
     try:
         #print(row)
-        kw_cell = row[author_keywords_idx]
-        kw_cell2 = row[keywords_plus_idx]
-        if not kw_cell:
-            raise Exception(f"empty 'Author Keywords' at row {row_idx}")
-        if not kw_cell2:
-            raise Exception(f"empty 'Keywords Plus' at row {row_idx}")
-
-        kw_list = [k.strip() for k in kw_cell.split(';')]
-        for kw in kw_list:
-            lkw = kw.lower()
-            if lkw not in g_kw_feq.keys():
-                g_kw_feq[lkw] = 1
-            else:
-                g_kw_feq[lkw] += 1
-
-        kw_list = [k.strip() for k in kw_cell2.split(';')]
-        for kw in kw_list:
-            if kw not in g_kwp_feq.keys():
-                g_kwp_feq[kw] = 1
-            else:
-                g_kwp_feq[kw] += 1
+        proc_one_row(row)
     except Exception as e:
-        print(f'[WARN] row {row_idx} has unexpected format. ErrCode:{repr(e)}')
+        pwarn(f'row {row_idx} has unexpected format. ErrCode:{repr(e)}')
 
-result_sheet = wb.create_sheet("Result_Author_Keyword")
-for pair in g_kw_feq.items():
-    result_sheet.append(pair)
-
-result_sheet = wb.create_sheet("Result_Keyword_Plus")
-for pair in g_kwp_feq.items():
-    result_sheet.append(pair)
-wb.save(FILE_NAME)
+print(f"parsing done. hit {len(g_warnings)} warnings")
+save_keyword_feq_to_result_sheet(wb)
+wb.close()
+print("procedure complete. ^_^")
